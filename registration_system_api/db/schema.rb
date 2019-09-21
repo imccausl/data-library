81f28fbaaa85944e235c2fda7454ee2f6ac24868
Mandeep <mkaur@pagerduty.com>
2019-09-21 15:34:53 -0400

    set up seeds

--- registration_system_api/db/schema.rb
+++ registration_system_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_183818) do
+ActiveRecord::Schema.define(version: 2019_09_21_185650) do
 
   create_table "devices", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_09_21_183818) do
     t.integer "deposit_amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
   end
 
   create_table "plans", force: :cascade do |t|
