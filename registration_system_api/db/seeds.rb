95d9a2812caeca141f76cb931653b7855a53fe88
Ian McCausland <imccausl@gmail.com>
2019-09-22 11:14:36 -0400

    Change db seed

--- registration_system_api/db/seeds.rb
+++ registration_system_api/db/seeds.rb
@@ -4,7 +4,11 @@
 # Examples:
 #
   User.create(name: 'Beckie Books', library_card_number: '17a9s9xa3jsss' )
-  device = Device.create(name: "Roger's Personal Hotspot", location: 'Toronto', deposit_amount: 20, url: 'https://www.rogers.com/cms/rogers-smb-new/page-specific/products-and-solutions/mobile-internet-plans/Images/550x300_ZTEhotspot.png')
-  Plan.create(name: 'My3', device_id: device.id, restrictions: 'No Social Media, No Streaming, No Bueno', gigabytes: 3, cost: 20 )
-  Plan.create(name: 'My5', device_id: device.id, restrictions: 'No Social Media, No Streaming, No Bueno', gigabytes: 3, cost: 30 )
-  Plan.create(name: 'My10', device_id: device.id, restrictions: 'No Social Media, No Streaming, No Bueno', gigabytes: 3, cost: 50 )
+
+  device = Device.create(id: 55555, name: "Roger's Personal Hotspot", location: 'Toronto', deposit_amount: 20, url: 'https://www.rogers.com/cms/rogers-smb-new/page-specific/products-and-solutions/mobile-internet-plans/Images/550x300_ZTEhotspot.png')
+  device2 = Device.create(id: 7448, name: "Arel's Hotspot", location: 'Toronto', deposit_amount: 20, url: 'https://www.rogers.com/cms/rogers/page-specific/wireless/mobile-internet/images/novatel-mifi7000.png')
+  device3 = Device.create(id: 123456, name: "Will's Mobile Hotspot", location: 'Toronto', deposit_amount: 20, url: 'https://www.rogers.com/cms/rogers-smb-new/page-specific/products-and-solutions/mobile-internet-plans/Images/550x300_ZTEhotspot.png')
+
+  Plan.create(name: 'MY3', device_id: device.id, restrictions: 'Basic Plan', gigabytes: 3, cost: 5 )
+  Plan.create(name: 'MY5', device_id: device2.id, restrictions: 'Better Plan', gigabytes: 5, cost: 10 )
+  Plan.create(name: 'MY10', device_id: device3.id, restrictions: 'Best Plan', gigabytes: 10, cost: 20 )
